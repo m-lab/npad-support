fc19afa1941b1017a73901414d3ff29d915fedbb
Greg Russell <gfr10598@gmail.com>
2017-03-01 13:04:20 -0500

    Update deployment rules to match README

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,13 +3,12 @@ set -x
 set -e
 
 KEYFILE=${1:?Please provide the service account key file}
-FOLDER=${2:-new}
+PATH=${2:?Please provide destinate gs://PATH}
 
 $TRAVIS_BUILD_DIR/travis/install_gcloud.sh
 
 # Add gcloud to PATH.
 source "${HOME}/google-cloud-sdk/path.bash.inc"
-
 
 # All operations are performed as the service account named in KEYFILE.
 # For all options see:
@@ -17,10 +16,10 @@ source "${HOME}/google-cloud-sdk/path.bash.inc"
 gcloud auth activate-service-account --key-file "${KEYFILE}"
 
 # For this to succeed, the provided keyfile must provide W access to the
-# legacy-rpms-mlab-sandbox bucket.  This is arranged by executing this from
+# specified bucket.  This is arranged by executing this from
 # a privileged account:
-# gsutil acl ch -u legacy-rpm-writer@mlab-sandbox.iam.gserviceaccount.com:W \
-#  gs://legacy-rpms-mlab-sandbox
-gsutil cp slicebase-i386/i686/*.rpm gs://legacy-rpms-mlab-sandbox/$FOLDER/
+# gsutil acl ch -u legacy-rpm-writer@PROJECT.iam.gserviceaccount.com:W \
+#  gs://BUCKET
+gsutil cp slicebase-i386/i686/*.rpm gs://${PATH}/
 
 exit 0
